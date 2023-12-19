ec4985b62a6c54c3bd145336ea17cf048e7d6e19
anildalar <dollorinfotech@gmail.com>
2023-12-19 09:10:27 +0530

    done

---
+++ test.py
@@ -0,0 +1,25 @@
+class SecondClass():
+    
+    
+    #.3 
+    def addMenu(self,menuTitle):
+        print("Calculator")
+        pass
+    pass
+class Anil():
+    #1.
+    
+    #2
+    
+    #3
+    def menuBar(self):
+        #print("MenuBar is called")
+        co2 = SecondClass()
+        return co2
+        pass
+    pass
+
+
+co = Anil()
+mb = co.menuBar()
+print(mb.addMenu("Calcultor"))
